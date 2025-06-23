eadd100bcb35115529199f15c35460c482cebe44
Hayashi Masahiro <gfekimae523@gmail.com>
2025-06-23 12:56:59 +0900

    背景のフェード処理の実装

--- js/index.js
+++ js/index.js
@@ -16,6 +16,20 @@ window.addEventListener('load', () => {
     window.addEventListener('scroll', animateOnScroll);
     animateOnScroll(); // 初期実行
 
+    const images = document.querySelectorAll('.hero__bg-image');
+    let current = 0;
+
+    function showNextImage() {
+        console.log("hoge");
+        images[current].classList.remove('active');
+        current = (current + 1) % images.length;
+        images[current].classList.add('active');
+    }
+
+    // 初期表示
+    images[current].classList.add('active');
+    setInterval(showNextImage, 5000); // 5秒ごとに切り替え
+
     // スムーススクロール
     // document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     //     anchor.addEventListener('click', function (e) {
